acdd28fc3a05f1b32903a980936f8eb426744092
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2022-12-25 21:46:27 -0300

    Add files via upload

--- src/aval20222/q1/Conversor.java
+++ src/aval20222/q1/Conversor.java
@@ -6,3 +6,5 @@ public interface Conversor {
    public String converte(Obra obra);
    public Conversor clone();
 }
+
+//Essa é a primeira questão do trabalho, nós utilizamos o padrão prototype, onde temos a interface Conversor, onde as classes concretas ConversorJSON e ConversorXML implementam a interface Conversor. 
